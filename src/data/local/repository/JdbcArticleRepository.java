89493314b66958a62a8c412076c62850a82d673e
Egor <gocamper03@gmail.com>
2026-09-22 20:50:17 +0300

    CRUD operations for the Article model - JDBC

--- src/data/local/repository/JdbcArticleRepository.java
+++ src/data/local/repository/JdbcArticleRepository.java
@@ -1,5 +1,6 @@
 package data.local.repository;
 
+import com.fasterxml.jackson.databind.ext.SqlBlobSerializer;
 import data.local.database.DatabaseConnectionFactory;
 import domain.model.Article;
 import domain.repository.ArticleRepository;
@@ -17,16 +18,79 @@ public class JdbcArticleRepository implements ArticleRepository {
 
     @Override
     public void addArticle(Article article) {
+        String sql = """
+                        INSERT INTO articles (author_id, status_id, title, content, published_at)
+                        VALUES (?, (SELECT id FROM article_statuses WHERE code = ?), ?, ?, ?)
+                        RETURNING id
+                """;
+
+        try (
+                var connection = connectionFactory.openConnection();
+                var statement = connection.prepareStatement(sql);
+        ) {
+            statement.setInt(1, article.getAuthorId());
+            statement.setString(2, article.getStatus().name());
+            statement.setString(3, article.getTitle());
+            statement.setString(4, article.getContent());
+            statement.setString(5, article.getPublishedAt());
+
+            try (var result = statement.executeQuery()) {
+                if (!result.next()) throw new IllegalStateException("Database didn't return anything");
+
+                article.setId(result.getInt("id"));
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException("Couldn't add article");
+        }
     }
 
     @Override
     public void deleteArticle(int articleId) {
+        String sql = """
+                DELETE FROM articles WHERE id = ?
+                """;
 
+        try (var connection = connectionFactory.openConnection();
+             var statement = connection.prepareStatement(sql);
+        ) {
+            statement.setInt(1, articleId);
+
+            int affectedRows = statement.executeUpdate();
+
+            if (affectedRows == 0) throw new IllegalArgumentException("No article found");
+
+
+        } catch (SQLException e) {
+            throw new IllegalStateException("Couldn't delete article");
+        }
     }
 
     @Override
     public void editArticle(Article article) {
+        String sql = """
+                UPDATE articles
+                SET
+                    status_id = (SELECT id FROM article_statuses WHERE code = ?),
+                    title = ?,
+                    content = ?
+                WHERE id = ?
+                """;
 
+        try (var connection = connectionFactory.openConnection();
+             var statement = connection.prepareStatement(sql);
+        ) {
+            statement.setString(1, article.getStatus().name());
+            statement.setString(2, article.getTitle());
+            statement.setString(3, article.getContent());
+            statement.setInt(4, article.getId());
+
+            int affectedRows = statement.executeUpdate();
+
+            if (affectedRows == 0) throw new IllegalArgumentException("Couldn't find article");
+
+        } catch (SQLException e) {
+            throw new IllegalStateException("Couldn't edit article");
+        }
     }
 
     @Override
@@ -36,7 +100,41 @@ public class JdbcArticleRepository implements ArticleRepository {
 
     @Override
     public Article getArticleById(int articleId) {
-        return null;
+        String sql = """
+                SELECT
+                    article.id,
+                    article.author_id,
+                    article.title,
+                    article.content,
+                    status.code AS status_code,
+                    article.published_at
+                FROM articles AS article
+                JOIN article_statuses AS status ON status.id = article.status_id
+                WHERE article.id = ?;
+                """;
+
+        try (var connection = connectionFactory.openConnection();
+             var statement = connection.prepareStatement(sql);
+        ) {
+            statement.setInt(1, articleId);
+
+            try (var result = statement.executeQuery()) {
+                if (!result.next()) throw new IllegalArgumentException("Couldn't find an article");
+
+                Article returnArticle = new Article(result.getInt("id"),
+                        result.getInt("author_id"),
+                        result.getString("title"),
+                        result.getString("content"),
+                        Article.Status.valueOf(result.getString("status_code")),
+                        result.getString("published_at"));
+
+                return returnArticle;
+            }
+
+        } catch (SQLException e) {
+            throw new IllegalStateException("Couldn't get the article by id");
+        }
+
     }
 
     @Override
